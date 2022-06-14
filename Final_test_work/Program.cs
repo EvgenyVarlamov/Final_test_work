48f1cbcf282a090ad298cbb96ed6b7a4fd012c08
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-06-14 23:23:45 +0300

    add Program(C#)

--- Final_test_work/Program.cs
+++ Final_test_work/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿string[] initialArray = { "Написать", "программу", ",", "которая", "из", "имеющегося", "массива", "строк", "формирует",
+                          "массив", "из", "строк", ",", "длина", "которых", "меньше", "либо", "равна", "3", "символа" };
+int arrayElementSize = 3;
+
+int ArrayLengthCalculation(int symbolsQuantity, string[] inputArray)
+{
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= symbolsQuantity)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] ArrayCreationFromInitialArray(int symbolsQuantity, string[] inputArray, int newArrayLength)
+{
+    string[] outputArray = new string[newArrayLength];
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= symbolsQuantity)
+        {
+            outputArray[count] = initialArray[i];
+            count++;
+        }
+    }
+    return outputArray;
+}
+
+string PrintArray(string message, string[] inputArray)
+{
+    Console.Write(message);
+    string output = "[ ";
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        output += $"'{inputArray[i]}' ";
+    }
+    output += " ]";
+    return output;
+}
+
+int arrayLength = ArrayLengthCalculation(arrayElementSize, initialArray);
+string[] targetArray = ArrayCreationFromInitialArray(arrayElementSize, initialArray, arrayLength);
+Console.WriteLine(PrintArray("Исходный массив: ", initialArray));
+Console.WriteLine(PrintArray("Целевой массив: ", targetArray));
